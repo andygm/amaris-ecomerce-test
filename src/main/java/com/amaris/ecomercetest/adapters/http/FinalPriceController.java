4201d734592605e7e2fe51e3546545bcc0370fa9
Andy González Méndez <andy.gonzalez@nearsure.net>
2022-05-04 00:41:47 -0400

    Deployment
    Added swagger descriptions to FinalPriceController
    Fixed warnings in ExceptionMapper

--- src/main/java/com/amaris/ecomercetest/adapters/http/FinalPriceController.java
+++ src/main/java/com/amaris/ecomercetest/adapters/http/FinalPriceController.java
@@ -3,6 +3,8 @@ package com.amaris.ecomercetest.adapters.http;
 import com.amaris.ecomercetest.adapters.http.dto.PriceDto;
 import com.amaris.ecomercetest.adapters.http.dto.mapper.PriceMapper;
 import com.amaris.ecomercetest.app.service.PriceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import reactor.core.publisher.Mono;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 
+@Tag(name = "Price")
 @RestController
 @RequestMapping("/api/v1")
 public class FinalPriceController {
@@ -22,6 +25,7 @@ public class FinalPriceController {
         this.priceMapper = priceMapper;
     }
 
+    @Operation(summary = "Get final price for some product, in some brand for an specific date")
     @GetMapping("/brand/{brand}/product/{product}/price")
     public Mono<ResponseEntity<PriceDto>> getFinalPrice(@PathVariable BigInteger brand,
                                                         @PathVariable BigInteger product,
